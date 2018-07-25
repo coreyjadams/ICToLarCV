22046055c273ec37c5b304055b47e548ee8c1ab8
coreyjadams <coreyadams@fas.harvard.edu>
2018-07-25 16:13:33 -0500

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#setup root:
+source /home/cadams/Software/ROOT/root6/bin/thisroot.sh
+#setup IC: (this cd is necessary, great design!)
+cd /home/cadams/NEXT/IC/; source manage.sh work_in_python_version 3.6; cd -
+#setup larcv:
+source /home/cadams/NEXT/larcv2/configure.sh
